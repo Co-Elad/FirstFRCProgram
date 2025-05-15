619159a33c5f9ed29aeba4f46546afbdbba3044a
elao-co2010 <eladco2010@gmail.com>
2025-05-15 23:38:39 +0300

    add isValidDate function and isLeapYear function

--- src/main/java/org/example/Date.java
+++ src/main/java/org/example/Date.java
@@ -6,6 +6,12 @@ public class Date {
     private int month;
     private int year;
 
+    public Date (){
+        this.day = 1;
+        this.month = 1;
+        this.year = 2000;
+    }
+
 
     public Date (int day,int month,int year){
         this.day = day;
@@ -17,6 +23,44 @@ public class Date {
 
 
 
-    
+    public static boolean isValidDate(Date dateOfBirt){
+        if (dateOfBirt.month>12||dateOfBirt.month<1){
+            dateOfBirt = new Date();
+
+
+            return false;
+        }
+        else if (dateOfBirt.year<0){
+            dateOfBirt = new Date();
+
+            return false;
+        }
+
+        else if ((dateOfBirt.day<0|| dateOfBirt.day>31)){
+            dateOfBirt = new Date();
+
+            return false;
+        }
+        else if ((dateOfBirt.month == 4 || dateOfBirt.month == 6||dateOfBirt.month ==  9|| dateOfBirt.month == 11 && dateOfBirt.day > 30)) {
+            dateOfBirt = new Date();
+
+            return false;
+        }
+        else if ((dateOfBirt.month == 1 || dateOfBirt.month == 3||dateOfBirt.month ==  5|| dateOfBirt.month == 7|| dateOfBirt.month == 8 || dateOfBirt.month == 10 || dateOfBirt.month == 12&& dateOfBirt.day > 31)) {
+            dateOfBirt = new Date();
+
+            return false;
+        } else if ((dateOfBirt.isLeapYear(dateOfBirt.year)&& dateOfBirt.month == 2 && dateOfBirt.day >28)) {
+            dateOfBirt = new Date();
+            return false;
+        }
+        return true;
+
+
+    }
+
+    private boolean isLeapYear (int year){
+        return (year%4 == 0 && year%100!=0) || (year%400 == 0)? true:false;
+    }
 
 }
