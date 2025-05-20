2e1a4fd418330944483be96f825307e6f492cc84
elao-co2010 <eladco2010@gmail.com>
2025-05-20 18:48:27 +0300

    add equals lessThen

--- src/main/java/org/example/Weight.java
+++ src/main/java/org/example/Weight.java
@@ -5,20 +5,24 @@ public class Weight {
     private int Grams;
 
 
-    Weight( int Kg, int Grams){
+    Weight(int Kg, int Grams) {
         this.Kg = Kg = Kg += Grams / 1000;
         this.Grams = Grams = Grams % 1000;
     }
 
-    public  String toString(){
+    public String toString() {
         return "Kg " + Kg + "and gr " + Grams;
     }
 
-    public  boolean equals(Weight other){
-       ;
+    public boolean equals(Weight other) {
+
         return other.Grams == Grams || other.Kg == Kg;
     }
 
+    public boolean lessThen(Weight other) {
+
+        return other.Grams > Grams && other.Kg == Kg || other.Grams == Grams && other.Kg > Kg || other.Grams > Grams && other.Kg > Kg;
+    }
 
 
 }
